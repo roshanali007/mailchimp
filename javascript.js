2e89311835afbb85956168fc54cf720b3a0a1de0
Roshan Ali <roshanalink321@gmail.com>
2025-06-12 06:00:06 -0700

    jun12 6.30pm

--- javascript.js
+++ javascript.js
@@ -1,3 +1,37 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const navBar = document.querySelector(".nav_bar");
+    const navBody = document.querySelector(".nav_body"); 
+    const initialNavBarOffset = navBar.offsetTop; 
+
+    
+    let navBarHeight = navBody.offsetHeight;
+
+    function stickyNav() {
+        if (window.scrollY >= initialNavBarOffset) {
+            navBar.classList.add("sticky");
+            
+            if (document.body.style.paddingTop === "" || document.body.style.paddingTop === "0px") {
+                 document.body.style.paddingTop = navBarHeight + "px";
+            }
+        } else {
+            navBar.classList.remove("sticky");
+            
+            document.body.style.paddingTop = "0";
+        }
+    }
+
+    
+    window.addEventListener("scroll", stickyNav);
+
+    
+    stickyNav();
+
+    
+    window.addEventListener("resize", function() {
+        navBarHeight = navBody.offsetHeight;
+        stickyNav();
+    });
+});
 function showsidebar(){
     const sidebar=document.querySelector('.sidebar')
     sidebar.style.display = 'flex'
